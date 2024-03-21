84d1bdc747c971a1a17188edc6425789c648c6f7
Jackson Fidelix <jcksfidelix@gmail.com>
2024-03-21 09:55:52 -0300

    commit de atualização do projeto - Calculadoras
    Criei duas calculadoras - sendo, manual e automática e fiz alguns ajustes nas telas quanto a responsividade

---
+++ js/calc.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('#calcular-btn').click(function() {
+        var tvConsumo = parseFloat($('#tv').val() || 0);
+        var geladeiraConsumo = parseFloat($('#geladeira').val() || 0);
+        var arCondicionadoConsumo = parseFloat($('#ar-condicionado').val() || 0);
+        var maqRoupaConsumo = parseFloat($('#maq-roupa').val() || 0);
+        var chuveiroConsumo = parseFloat($('#chuveiro').val() || 0);
+        var microOndasConsumo = parseFloat($('#micro-ondas').val() || 0);
+
+        // Calcula a demanda total somando o consumo de todos os equipamentos
+        var demandaTotal = tvConsumo + geladeiraConsumo + arCondicionadoConsumo + maqRoupaConsumo + chuveiroConsumo + microOndasConsumo;
+
+        // Calcula o número de placas solares necessárias, assumindo que uma placa gera cerca de 300W
+        var numeroPlacas = Math.ceil(demandaTotal / 300);
+
+        // Exibe o resultado na div com id "resultado"
+        $('#resultado').text('Você precisaria de aproximadamente ' + numeroPlacas + ' placas solares.');
+    });
+});
+
+// Funções JavaScript atualizadas
+function insert(num) {
+    var numero = document.getElementById('resultado_calc_').innerHTML;
+    document.getElementById('resultado_calc_').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('resultado_calc_').innerHTML = ""; // Limpa para 0
+}
+
+function back() {
+    var resultado = document.getElementById('resultado_calc_').innerHTML;
+    document.getElementById('resultado_calc_').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcule() {
+    var result = document.getElementById('resultado_calc_').innerHTML;
+    if (result.trim() !== '') {
+        document.getElementById('resultado_calc_').innerHTML = eval(result);
+    } else {
+        document.getElementById('resultado_calc_').innerHTML = 'Entrada Inválida!';
+    }
+}
+
